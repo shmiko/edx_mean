0d4117ce9c1d5b25e965718e0807dfc843079b6c
Paul Jones <shmiko@gmail.com>
2016-02-10 23:26:48 +1100

    edx lesson 1

---
+++ chapter1/interface.js
@@ -0,0 +1,24 @@
+/*
+ *  Inserts "doc" into the collection "movies".
+ */
+exports.insert = function(db, doc, callback) { db.collection('movies').insert(doc, function(error,result){ if (error) { console.log(error); process.exit(1);}; callback(null); });
+
+
+// db.collection('movies').insert(doc, function(error, result) { // <-- note 'result', not 'db'
+//   if (error) {
+//     console.log(error);
+//     process.exit(1);
+//   }
+// });
+/*
+ *  Finds all documents in the "movies" collection
+ *  whose "director" field equals the given director,
+ *  ordered by the movie's "title" field. See
+ *  http://mongodb.github.io/node-mongodb-native/2.0/api/Cursor.html#sort
+ */
+exports.byDirector = function(db, director, callback) {
+db.collection('movies').find({'director': director}).sort({'title':1}).toArray(function(error,docs){
+if (error){ console.log(error); process.exit(1); }
+else { callback(null, docs); }
+});
+};
