de4c54aecbc90af2a8c574337580ce74e292e69a
Paul Jones <shmiko@gmail.com>
2016-02-20 23:59:16 +1100

    testing api working ok now.

--- chapter-4/server/api.js
+++ chapter-4/server/api.js
@@ -43,7 +43,7 @@ module.exports = function(wagner) {
     };
   }));
 
-  /* Product API 11773 ok works*/
+  /* Product API 117734 ok works*/
   api.get('/product/id/:id', wagner.invoke(function(Product) {
     return function(req, res) {
       Product.findOne({ _id: req.params.id },
