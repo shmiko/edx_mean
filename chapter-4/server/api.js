41cfc1dbde11cdca1c344ab146e87ae727c7b7a7
Paul Jones <shmiko@gmail.com>
2016-02-20 23:57:38 +1100

    testing api ok

--- chapter-4/server/api.js
+++ chapter-4/server/api.js
@@ -43,7 +43,7 @@ module.exports = function(wagner) {
     };
   }));
 
-  /* Product API  ok*/
+  /* Product API  ok work*/
   api.get('/product/id/:id', wagner.invoke(function(Product) {
     return function(req, res) {
       Product.findOne({ _id: req.params.id },
